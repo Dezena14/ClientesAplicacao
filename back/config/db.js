a7a7cea5b8e50311c6dfd70e74fcaeb6a6da9482
Eduardo Dezena <73503077+Dezena14@users.noreply.github.com>
2024-08-29 15:32:53 -0300

    changed db

--- back/config/db.js
+++ back/config/db.js
@@ -3,11 +3,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',         // Nome do usuário do banco de dados
-  host: 'localhost',        // Endereço do host do banco de dados
-  database: 'clientes',      // Nome do banco de dados
-  password: 'root',         // Senha do banco de dados
-  port: 5432,               // Porta do banco de dados
+  user: 'postgres',                                                         // Nome do usuário do banco de dados
+  host: 'database-cliente.cb8ese40u5q9.us-east-1.rds.amazonaws.com',        // Endereço do host do banco de dados
+  database: 'clientes',                                                     // Nome do banco de dados
+  password: 'i1QaflXTHwDRo0NgU1Xo',                                         // Senha do banco de dados
+  port: 5432,                                                               // Porta do banco de dados
 });
+
 
 module.exports = pool;
