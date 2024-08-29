31d0c8ce2f290e957f85524319008b9bb4300a34
barbarafrc <64804523+barbarafrc@users.noreply.github.com>
2024-08-29 17:27:31 -0300

    new database settigns

--- back/config/db.js
+++ back/config/db.js
@@ -3,12 +3,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',                                                         // Nome do usuário do banco de dados
-  host: 'database-cliente.cb8ese40u5q9.us-east-1.rds.amazonaws.com',        // Endereço do host do banco de dados
-  database: 'clientes',                                                     // Nome do banco de dados
-  password: 'i1QaflXTHwDRo0NgU1Xo',                                         // Senha do banco de dados
-  port: 5432,                                                               // Porta do banco de dados
+  user: 'teste',         // Nome do usuário do banco de dados
+  host: 'clientes2.cb8ese40u5q9.us-east-1.rds.amazonaws.com',        // Endereço do host do banco de dados
+  database: 'clientes',      // Nome do banco de dados
+  password: 'S2OaO78v6pMJMFGa2dPz',         // Senha do banco de dados
+  port: 5432,               // Porta do banco de dados
 });
-
 
 module.exports = pool;
