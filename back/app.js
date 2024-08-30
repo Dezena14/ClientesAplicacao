f49d64c36402a1b495b60696f21178a9a0e90828
Eduardo Dezena <73503077+Dezena14@users.noreply.github.com>
2024-08-30 13:13:13 -0300

    added cords

--- back/app.js
+++ back/app.js
@@ -5,6 +5,12 @@ const express = require('express');
 const clienteRoutes = require('./routes/clienteRoutes');
 
 const app = express();
+
+app.use(cors()); // Permite requisições de qualquer origem
+
+// Para uma origem específica
+// app.use(cors({ origin: 'http://44.223.28.193:81' }));
+
 app.use(express.json());
 
 app.use('/clientes', clienteRoutes);
